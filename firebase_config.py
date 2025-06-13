b6f6a3ceac6009385d10ac082d0b09addb14b3f6
mrnithesh <mr.nithesh.k@gmail.com>
2025-06-13 18:43:37 +0530

    fix #3

--- firebase_config.py
+++ firebase_config.py
@@ -2,12 +2,18 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 from pathlib import Path
 from config import FIREBASE_CONFIG
+import json
 
 def initialize_firebase():
     """Initialize Firebase Admin SDK and return Firestore client."""
     if not firebase_admin._apps:
         try:
-            cred = credentials.Certificate(str(FIREBASE_CONFIG["credentials_path"]))
+            creds_json_str = FIREBASE_CONFIG["credentials"]
+            if not creds_json_str:
+                raise ValueError("Missing Firebase credentials in environment variable.")
+            
+            cred_dict = json.loads(creds_json_str)
+            cred = credentials.Certificate(cred_dict)
             firebase_admin.initialize_app(cred, {
                 'projectId': FIREBASE_CONFIG["project_id"],
                 'storageBucket': FIREBASE_CONFIG["storage_bucket"],
